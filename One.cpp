38cc88c073897a66d6bdb6ca79918bb6060f83a0
kprahlad <31153973+kprahlad@users.noreply.github.com>
2017-08-19 13:15:06 +0530

    Update and rename README.cpp to One.cpp

---
+++ One.cpp
@@ -0,0 +1,14 @@
+//Assignment 1 - Prahlad Narasimhan 1711071
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    float b;
+    char c;
+    double d;
+    int *p=a;
+    cin>>a>>b>>c>>d;
+    cout<<a<<b<<c<<d<<p;
+    return 0;
+}
